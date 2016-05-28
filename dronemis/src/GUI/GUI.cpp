334400f558254df18c208f95f23269af84220243
andreas <a@andreaslorentzen.dk>
2016-05-28 23:49:38 +0200

    removed inclde and color

--- dronemis/src/GUI/GUI.cpp
+++ dronemis/src/GUI/GUI.cpp
@@ -6,7 +6,7 @@
 void GUI::video(sensor_msgs::ImageConstPtr img)
 {
 
-      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::MONO8);
+      cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::RGB16);
 
       cv::imshow("TEST", cv_ptr->image);
       cv::waitKey(10);
