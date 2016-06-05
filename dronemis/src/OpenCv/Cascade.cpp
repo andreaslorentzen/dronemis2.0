32e303716f10cadb624c543ede7e965e54ae09c5
cgenter@oncable.dk <Hippomormor80>
2016-06-06 01:22:47 +0200

    Added setCascade and started checkCascade.. Added constructor

--- dronemis/src/OpenCv/Cascade.cpp
+++ dronemis/src/OpenCv/Cascade.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "Cascade.h"
+
+using namespace cv;
+
+void Cascade::Cascade() {
+
+    if(!cascade_classifier.load("cascades/cascade700.xml")){
+        ROS_INFO("ERROR LOADING DEFAULT CASCADE..");
+    };
+}
+
+bool Cascade::setCascade(const String cascadeName) {
+
+    if(!cascade_classifier.load(cascadeName)){
+        ROS_INFO("ERROR LOADING CASCADE..");
+        return false;
+    };
+}
+
+
+int Cascade::checkCascade( sensor_msgs::ImageConstPtr img )
+{
+    std::vector<Rect> cubes;
+    Mat frame_modified;
+
+    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::MONO8);
+
+    //cvtColor( cv_ptr->image, frame_gray, CV_BGR2GRAY ); // hmmm
+    equalizeHist( cv_ptr->image, frame_modified );
+
+    //-- Detect cubes
+    cascade_classifier.detectMultiScale( frame_modified, cubes, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+
+    for( size_t i = 0; i < cubes.size(); i++ )
+    {
+        Point center( cubes[i].x + cubes[i].width*0.5, cubes[i].y + cubes[i].height*0.5 );
+        ellipse( cv_ptr->image, center, Size( cubes[i].width*0.5, cubes[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+
+    }
+}
--- dronemis/src/OpenCv/Cascade.h
+++ dronemis/src/OpenCv/Cascade.h
@@ -5,10 +5,23 @@
 #ifndef PROJECT_CASCADE_H
 #define PROJECT_CASCADE_H
 
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+#include <ros/ros.h>
 
 class Cascade {
 
-};
+private:
+    cv::CascadeClassifier cascade_classifier;
 
+public:
+    void Cascade::Cascade();
+    bool Cascade::setCascade(const cv::String cascadeName);
+    int checkCascade(sensor_msgs::ImageConstPtr img);
+};
 
 #endif //PROJECT_CASCADE_H
