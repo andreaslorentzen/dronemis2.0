b88d7ab5e72abed62b9576e6afee4967dfb2b8d4
cgenter@oncable.dk <Hippomormor80>
2016-06-08 03:33:46 +0200

    Added unique boost mutex to control remotely. Added memcopy of image (in BGR 8 bit  3 channel). Added proof of concept conversion from CVD data structure to OpenCV image.

--- dronemis/src/OpenCv/CV_Handler.cpp
+++ dronemis/src/OpenCv/CV_Handler.cpp
@@ -5,37 +5,64 @@
 #include "CV_Handler.h"
 #include "../GUI/VideoHandler.h"
 
+
 VideoHandler *videohandler;
 
-CV_Handler::CV_Handler(void)  {
+
+CV_Handler::CV_Handler(void) {
 
 }
 
-void CV_Handler::run(void){
+
+void CV_Handler::run(void) {
     cascade = new Cascade();
     videohandler = new VideoHandler();
 }
 
+
 CV_Handler::~CV_Handler(void) {
-    delete(cascade);
-    delete(videohandler);
+    delete (cascade);
+    delete (videohandler);
 }
+
 
 void CV_Handler::video(sensor_msgs::ImageConstPtr img) {
 
-    // Convert from ROS image message to OpenCV Mat with cv_bridge
-    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::MONO8);
 
-    cv::imshow("TEST", cv_ptr->image);
+    // Convert from ROS image message to OpenCV Mat with cv_bridge (unsigned 8 bit BGR)
+    cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
+
+    // Lock with unique lock (can be closed/opened remotely)
+    boost::unique_lock<boost::mutex> lock(new_frame_signal_mutex);
+
+    // Save size constraints to data structure
+    storedImage.resize(CVD::ImageRef(img->width, img->height));
+
+    // Copy image to CVD data structure
+    size_t size = img->width * img->height;
+    memcpy(storedImage.data(), cv_ptr->image.data,  size * 3);
+
+    // Convert CVD byte array to OpenCV matrix (use CV_8U format - unsigned 8 bit)
+
+    cv::Mat image(img->height, img->width,CV_8UC3, storedImage.data());
+
+    // Unlock mutex
+    lock.unlock();
+    new_frame_signal.notify_all();
+
+    cv::imshow("TEST", image);
     cv::waitKey(10);
+
 }
 
-CV_Handler::cascadeInfo** CV_Handler::checkColors(bool camera) {
+
+CV_Handler::cascadeInfo **CV_Handler::checkColors(bool camera) {
     return NULL;
 }
 
 
-CV_Handler::cascadeInfo** CV_Handler::checkCascades(bool camera) {
+CV_Handler::cascadeInfo **CV_Handler::checkCascades(bool camera) {
+    //boost::unique_lock<boost::mutex> lock(new_frame_signal_mutex);
     return NULL;
 }
 
