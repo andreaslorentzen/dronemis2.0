7a875217d247ec177a5b31ea677218f433c75880
Quayed <matsondrup@gmail.com>
2016-06-09 10:53:03 +0200

    Added main

--- dronemis/src/main.cpp
+++ dronemis/src/main.cpp
@@ -2,3 +2,92 @@
 // Created by mathias on 6/9/16.
 //
 
+#include "flightControl/blindFlight.h"
+#include "OpenCv/CV_Handler.h"
+
+#define NUM_THREADS 3
+#define LOOP_RATE (50)
+
+void *controlThread(void *thread_arg);
+void *buttonThread(void *thread_arg);
+void *cvThread(void *thread_arg);
+void *navdataThread(void *thread_Arg);
+
+bool started = false;
+FlightController *controller;
+CV_Handler *cvHandler;
+
+struct thread_data {
+    int argc;
+    char **argv;
+};
+
+struct thread_data td[NUM_THREADS];
+
+int main(int argc, char **argv){
+    td[0].argc = argc;
+    td[0].argv = argv;
+
+    ros::init(argc, argv, "blindFlight");
+    ros::NodeHandle n;
+
+    controller = new FlightController(LOOP_RATE, n);
+    cvHandler = new CV_Handler();
+
+    pthread_t threads[2];
+    pthread_create(&threads[0], NULL, buttonThread, &td[0]);
+    pthread_create(&threads[1], NULL, cvThread, &td[1]);
+
+    ros::spin();
+
+    pthread_exit(NULL);
+}
+
+void *controlThread(void *thread_arg){
+    controller->run();
+
+    pthread_exit(NULL);
+}
+
+void *navdataThread(void *thread_arg){
+
+}
+
+void *buttonThread(void *thread_arg) {
+
+    struct thread_data *thread_data;
+    thread_data = (struct thread_data *) thread_arg;
+
+    // Creating Control panel
+    QApplication a(thread_data->argc, thread_data->argv);
+    ControlPanel w;
+    w.show();
+    a.exec();
+
+    pthread_exit(NULL);
+}
+
+void *cvThread(void *thread_arg) {
+    cvHandler->run();
+
+    pthread_exit(NULL);
+}
+
+void blindFlight::abortProgram(void) {
+    ROS_INFO("MANUEL ABORT!");
+    controller->land();
+}
+
+void blindFlight::resetProgram(void) {
+    ROS_INFO("MANUEL RESET!");
+    controller->reset();
+}
+
+void blindFlight::startProgram(void) {
+    if (!started) {
+        ROS_INFO("STARTING!");
+        pthread_t thread;
+        pthread_create(&thread, NULL, controlThread, &td[0]);
+        started = true;
+    }
+}
