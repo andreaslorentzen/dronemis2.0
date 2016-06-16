ba778a5148d97538c5bf12141fd18be4963682c0
haxic <jageronline@gmail.com>
2016-06-16 11:37:27 +0200

    new nav

--- dronemis/src/navdata/Nav.cpp
+++ dronemis/src/navdata/Nav.cpp
@@ -9,6 +9,8 @@ void Nav::run(ros::NodeHandle *n) {
 
     ros::Subscriber sub_navdata = n->subscribe<ardrone_autonomy::Navdata>("ardrone/navdata", 5000, &Nav::navdataCallback, this);
     ros::Subscriber sub_magneto = n->subscribe<ardrone_autonomy::navdata_magneto>("ardrone/navdata_magneto", 5000, &Nav::magnetoCallback, this);
+    ros::Subscriber sub_imu = n->subscribe<sensor_msgs::Imu>("ardrone/imu", 5000, &Nav::imuCallback, this);
+
     ros::Subscriber sub_init = n->subscribe<std_msgs::Empty>("nav/init", 5000, &Nav::initCallback, this);
 
     while(ros::ok()){
@@ -24,13 +26,14 @@ void Nav::initCallback(const std_msgs::Empty::ConstPtr &msg){
 
     position.x = 0.0;
     position.y = 0.0;
+    x=0;
+    y=0;
 
 //    printf("running: %d",running);
 }
 int counter = 0;
 void Nav::navdataCallback(const ardrone_autonomy::Navdata::ConstPtr &msg) {
-    int counter_c = ++counter;
-    printf("%d\n",counter_c);
+
     state = msg->state;
 
 
@@ -42,17 +45,16 @@ void Nav::navdataCallback(const ardrone_autonomy::Navdata::ConstPtr &msg) {
     float ay = msg->ay;
 //    float aZ = msg->az;
 
+    /*
     position.z = msg->altd;
-/*
-     = (current_time()-last)/1000000000;
-    last = current_time();
-*/
+
     double curr = 0.005;
     position.x += vx * curr + 0.5 * ax * curr*curr;
     position.y += vy * curr + 0.5 * ay * curr*curr;
-
+*/
     std::string filename = "../workspaces/dronemis_ws/src/dronemis/src/navdata/log";
     filename.append(std::to_string(start_time));
+    filename.append("-1");
     filename.append(".csv");
 
     std::ofstream file;
@@ -79,16 +81,61 @@ void Nav::navdataCallback(const ardrone_autonomy::Navdata::ConstPtr &msg) {
 
 //    printf("s: %d\ta: %d\trot: %6.2f, %6.2f, %6.2f\tvel: %6.2f, %6.2f, %6.2f \tacc: %8.4f, %8.4f, %8.4f\n", state, altd, rX, rY, rZ, vX,vY,vZ, aX, aY, aZ);
 
-#ifdef DEBUG
-    printf("state: %d\t"
-                   "postition: %6.2f, %6.2f, %d\t"
-            "rotation: %6.2f\n", state, position.x, position.y, position.z, rotation);
-#endif
-    printf("%d\n",counter_c);
 
 }
 void Nav::magnetoCallback(const ardrone_autonomy::navdata_magneto::ConstPtr &msg) {
     rotation = msg->heading_fusion_unwrapped;
+}
+
+void Nav::imuCallback(const sensor_msgs::Imu::ConstPtr &msg) {
+
+
+    float vx = msg->angular_velocity.x;
+    float vy = msg->angular_velocity.y;
+//    float vZ = msg->vz;
+    float ax = msg->linear_acceleration.x;
+    float ay = msg->linear_acceleration.y;
+//    float aZ = msg->az;
+
+    position.z = 0.0;
+/*
+     = (current_time()-last)/1000000000;
+    last = current_time();
+*/
+
+
+
+    double curr = 0.005;
+    position.x += vx * curr + 0.5 * ax * curr*curr;
+    position.y += vy * curr + 0.5 * ay * curr*curr;
+
+    x += vx * curr + 0.5 * ax * curr*curr;
+    y += vy * curr + 0.5 * ay * curr*curr;
+
+    std::string filename = "../workspaces/dronemis_ws/src/dronemis/src/navdata/log";
+    filename.append(std::to_string(start_time));
+    filename.append("-2");
+    filename.append(".csv");
+
+    std::ofstream file;
+    file.open (filename, std::ios::app);
+    file << state;
+    file << ";";
+    file << position.x;
+    file << ";";
+    file << position.y;
+    file << ";";
+    file << ax;
+    file << ";";
+    file << ay;
+    file << ";";
+    file << vx;
+    file << ";";
+    file << vy;
+    file << "\n";
+    file.close();
+
+
 }
 
 
@@ -97,6 +144,8 @@ Nav::Nav() {
     running = 0;
     position.x = 0.0;
     position.y = 0.0;
+    x=0;
+    y=0;
     start_time = (int) ros::Time::now().toSec();
 }
 
--- dronemis/src/navdata/Nav.h
+++ dronemis/src/navdata/Nav.h
@@ -13,6 +13,7 @@
 #include <geometry_msgs/Twist.h>
 #include "std_msgs/String.h"
 #include "ros/callback_queue.h"
+#include <sensor_msgs/Imu.h>
 
 class Nav {
 private:
@@ -29,10 +30,13 @@ public:
         int z;
 
     } position;
+    double x, y;
+
     float rotation;
 
     void navdataCallback(const ardrone_autonomy::Navdata::ConstPtr &msg);
     void magnetoCallback(const ardrone_autonomy::navdata_magneto::ConstPtr &msg);
+    void imuCallback(const sensor_msgs::Imu::ConstPtr &msg);
     void initCallback(const std_msgs::Empty::ConstPtr &msg);
     Nav();
     void run(ros::NodeHandle *n);
