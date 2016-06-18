7732c4379c48a7004bb1b205491748ed8b86414c
cgenter@oncable.dk <Hippomormor80>
2016-06-18 02:32:57 +0200

    Disabled all debugs

--- dronemis/src/debug.h
+++ dronemis/src/debug.h
@@ -7,9 +7,9 @@
 
 //#define DEBUG 1
 //#define DEBUG_COUT 1
-#define DEBUG_CV_COUT 1
+//#define DEBUG_CV_COUT 1
 //#define DEBUG_NAV_COUT 1
-#define DEBUG_NAV_LOG 1
+//#define DEBUG_NAV_LOG 1
 
 #endif //PROJECT_DEBUG_H
     
